6767dafce6c973f07d98b6eb2c3c8b0a4c85add1
Maksim <maksim6erbinin@mail.ru>
2022-07-10 22:45:10 +0300

    Aded task 36

---
+++ Task_36/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите кол-во элементов массива: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] collecton) 
+{
+    for(int i = 0; i < collecton.Length; i++) collecton[i] = new Random().Next(-99,100);
+}
+
+int UnevenSumArray(int[] collecton, int unevenSum)
+{
+    for(int i = 0; i < collecton.Length; i++) if(i % 2 != 0) unevenSum += collecton[i];
+    return unevenSum;
+}
+
+int[] array = new int[numbers];
+int unevenSum = 0;
+FillArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine("Сумма элементов с нечётным индексом = {0}", UnevenSumArray(array, unevenSum));
