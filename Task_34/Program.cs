6f9d50759e2dd29f80a626faaab47df768a58659
Maksim <maksim6erbinin@mail.ru>
2022-07-10 22:04:34 +0300

    Added task 34

---
+++ Task_34/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите кол-во элементов массива: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+void FillArray(int[] collecton) 
+{
+    for(int i = 0; i < collecton.Length; i++) collecton[i] = new Random().Next(100,1000);
+}
+
+int EvenArray(int[] collecton, int even)
+{
+    for(int i = 0; i < collecton.Length; i++) if(collecton[i] % 2 == 0) even++;
+    return even;
+}
+
+int[] array = new int[numbers];
+int even = 0;
+FillArray(array);
+Console.WriteLine("[" + string.Join(", ", array) + "]");
+Console.WriteLine("В массиве {0} четных чисел(а)", EvenArray(array, even));
